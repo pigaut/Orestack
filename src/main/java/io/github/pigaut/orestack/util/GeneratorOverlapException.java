dd4836d3ac76148b0908c70c737bcc66f6a84285
Pigauts <sorrentinovirgilio@gmail.com>
2025-08-16 18:32:48 +0200

    version 3.29.4
    - Invalid generators (missing world, overlapped, template missing/broken) are now saved to the database and wait to be restored if they ever become valid again.

--- src/main/java/io/github/pigaut/orestack/util/GeneratorOverlapException.java
+++ src/main/java/io/github/pigaut/orestack/util/GeneratorOverlapException.java
@@ -1,9 +1,13 @@
 package io.github.pigaut.orestack.util;
 
-public class GeneratorOverlapException extends Exception {
+public class GeneratorOverlapException extends GeneratorCreateException {
 
     public GeneratorOverlapException() {
-        super("Cannot create generator because it overlaps with another.");
+        super("Failed to create generator. Reason: location is already occupied by another generator.");
+    }
+
+    public GeneratorOverlapException(String world, int x, int y, int z) {
+        super(world, x, y, z, "location is already occupied by another generator");
     }
 
 }
